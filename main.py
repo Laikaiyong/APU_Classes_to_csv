e267c78454b8c787e1f521e37b6f2098c57115c3
Vandyck <vandycklai@gmail.com>
2023-05-11 16:49:17 +0800

    Add exlucde classes logic

--- main.py
+++ main.py
@@ -3,13 +3,34 @@ import json
 from datetime import datetime, date
 import csv
 
+EXCLUDE_CLASSES = [
+    "CCP",
+    "FWDD",
+    "BMK2"
+]
+
 today = date.today()
 api_url = "https://s3-ap-southeast-1.amazonaws.com/open-ws/weektimetable"
 
 response = requests.get(url=api_url)
 
 timeschedule = json.loads(response.text)
-classes = [d for d in timeschedule if d['INTAKE'] == "APD2F2211SE"]
+# classes = [
+#     d
+#     for d in timeschedule
+#     if d['INTAKE'] == "APD2F2211SE"
+# ]
+
+classes = []
+for schedule in timeschedule:
+    if schedule['INTAKE'] == "APD2F2211SE":
+        proceedable: bool = True
+        for class_code in EXCLUDE_CLASSES:
+            if class_code in schedule["MODID"]:
+                proceedable = False
+                
+        if proceedable: classes.append(schedule)
+
 
 headers = ["Course", "Date", "From", "To"]
 filename = f'Vandyck_classes({today}).csv'
